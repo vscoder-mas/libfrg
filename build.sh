bce858b1b75df3709fc94528fb3ced3bb40c7bfe
zvezdochiot <mykaralw@yandex.ru>
2019-12-15 09:27:48 +0300

    1.3.2: added png2frg

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 
 PNAME="fgr"
 PDEMO="${PNAME}_demo"
+PP2F="png2${PNAME}"
 
 g++ -Wall reader/*.c writer/frg_private/bgr_zip/*.cpp writer/frg_private/*.cpp writer/*.cpp demo/frg_demo.cpp -o "${PDEMO}" -llz4
+g++ -Wall reader/*.c writer/frg_private/bgr_zip/*.cpp writer/frg_private/*.cpp writer/*.cpp demo/png2frg.cpp -o "${PP2F}" -llz4 -lpng15
